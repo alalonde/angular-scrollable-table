b3a3f0d4aaacf286b0cd87be158aedaef9f3395d
Alec LaLonde <alec@boxelderweb.com>
2016-05-31 13:15:41 -0600

    Fixed horizontal scrolling in Firefox, #41

--- angular-scrollable-table.js
+++ angular-scrollable-table.js
@@ -1,5 +1,7 @@
 (function (angular) {
   'use strict';
+
+  var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
   angular.module('scrollable-table', [])
     .directive('scrollableTable', ['$timeout', '$q', '$parse', function ($timeout, $q, $parse) {
       return {
@@ -162,8 +164,9 @@
           $scope.asc = !$attrs.hasOwnProperty("desc");
           $scope.sortAttr = $attrs.sortAttr;
 
+          var headerElementToFakeScroll = isFirefox ? "thead" : "thead th .th-inner";
           $element.find(".scrollArea").scroll(function (event) {
-            $element.find("thead th .th-inner").css('margin-left', 0 - event.target.scrollLeft);
+            $element.find(headerElementToFakeScroll).css('margin-left', 0 - event.target.scrollLeft);
           });
 
           $scope.$on("renderScrollableTable", function() {
